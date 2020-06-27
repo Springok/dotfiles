2d40512744ed5c44cd3db78c8ea2772da99558fb
spring <wenchuan.lin@abagile.com>
2020-06-27 14:29:11 +0800

    Add asdf support and setup

--- setup_env.sh
+++ setup_env.sh
@@ -23,6 +23,29 @@ stow --verbose git \
   zsh \
   nvim \
   tmux \
-  ruby
+  ruby \
+  asdf
+
+# Install asdf
+asdf_dir=$HOME/.asdf
+cd $HOME
+
+if [ ! -d $asdf_dir ]; then
+    echo "Installing asdf..."
+    git clone https://github.com/asdf-vm/asdf.git $asdf_dir
+    echo "asdf installation complete"
+else
+    echo "asdf already installed"
+fi
+
+asdf plugin-add java    || true
+asdf plugin-add clojure || true
+asdf plugin-add rust    || true
+asdf plugin-add python  || true
+asdf plugin-add golang  || true
+asdf plugin-add ruby    || true
+asdf plugin-add nodejs  || true
+
+asdf install
 
 exit 0
