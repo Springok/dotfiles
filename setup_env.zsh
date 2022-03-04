aeb20e89a5181c8420401abe20a796dee9ea1d79
spring <wenchuan.lin.tw@gmail.com>
2022-03-04 12:44:36 +0800

    Updates on setup script on 2022-03-04

--- setup_env.zsh
+++ setup_env.zsh
@@ -3,56 +3,51 @@
 if [[ "$(uname -s)" == "Darwin" ]]; then
   echo 'Mac OS X'
   brew install stow asdf fzf nvim tig tmux httpie htop bat
-
-  mkdir -p ~/.config/nvim
-
-  cd ~/dotfiles
-
-  stow --verbose git \
-    zsh \
-    nvim \
-    tmux \
-    clojure \
-    ruby \
-    asdf
-
-  # Install Vim Plug at first setup
-  sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
-  pip3 install pynvim
-
-  nvim +PlugInstall +qall
-
-  echo "You are all set!"
 else
   echo 'Linux (Untested)'
   sudo apt install stow bat
 
-  mkdir -p ~/.config/nvim
+  # https://github.com/neovim/neovim/wiki/Installing-Neovim#ubuntu
+  sudo add-apt-repository ppa:neovim-ppa/stable
+  sudo apt-get update
+  sudo apt-get install neovim
+  sudo apt-get install python-dev python-pip python3-dev python3-pip
 
-  stow --verbose git \
-    zsh \
-    nvim \
-    tmux \
-    clojure \
-    ruby \
-    asdf
-
-  # TODO: install asdf
   # Install asdf
   asdf_dir=$HOME/.asdf
   cd $HOME
 
   if [ ! -d $asdf_dir ]; then
     echo "Installing asdf..."
-    git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.0 $asdf_dir
+    git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.9.0 $asdf_dir
 
     source ~/.zshrc || true
     echo "asdf installation complete"
   else
     echo "asdf already installed"
   fi
-
-  echo "You are all set!"
 fi
+
+echo 'Setup Development Perferences (Nvim, zsh....)'
+
+mkdir -p ~/.config/nvim
+
+cd ~/dotfiles
+
+stow --verbose git \
+  zsh \
+  nvim \
+  tmux \
+  clojure \
+  ruby \
+  asdf
+
+# Install Vim Plug at first setup
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+pip3 install pynvim
+
+nvim +PlugInstall +qall
+
+echo "You are all set!"
