6cf8443593f726850a1f5a0de6b39df902f6e329
Wenchuan Lin <wenchuan.lin.tw@gmail.com>
2023-08-11 00:50:41 +0800

    Replace deprecated range_code_action after Nvim 9.0
    Reference:
      - https://github.com/neovim/neovim/pull/19551

--- nvim/.config/nvim/lua/config/lsp/init.lua
+++ nvim/.config/nvim/lua/config/lsp/init.lua
@@ -71,8 +71,7 @@ local function lsp_keymaps(bufnr)
   keymap("n", "<localleader>lt", vim.lsp.buf.type_definition, opts)
   keymap("n", "<localleader>lh", vim.lsp.buf.signature_help, opts)
   keymap("n", "<localleader>ln", vim.lsp.buf.rename, opts)
-  keymap("n", "<localleader>la", vim.lsp.buf.code_action, opts)
-  keymap("v", "<localleader>la", vim.lsp.buf.range_code_action, opts)
+  keymap({"v", "n"}, "<localleader>la", vim.lsp.buf.code_action, opts)
   keymap("n", "<localleader>lf", vim.lsp.buf.format, opts)
 
   vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting()' ]]
