d216c7ad2a424894902a35b1bfb29e77d98b554d
Wenchuan Lin <wenchuan.lin.tw@gmail.com>
2024-03-08 19:34:44 +0800

    Convert plugins setup to lazy nvim format

--- nvim/.config/nvim/lua/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -1,10 +1,3 @@
--- following options are the default
--- each of these are documented in `:help nvim-tree.OPTION_NAME`
-local status_ok, nvim_tree = pcall(require, "nvim-tree")
-if not status_ok then
-  return
-end
-
 local HEIGHT_RATIO = 0.8  -- You can change this
 local WIDTH_RATIO = 0.2   -- You can change this too
 
@@ -105,115 +98,126 @@ local function on_attach(bufnr)
   vim.keymap.set('n', 'm', api.fs.rename_sub, opts('Rename: Omit Filename'))
 end
 
-
-nvim_tree.setup {
-  on_attach = on_attach,
-  disable_netrw = true,
-  hijack_netrw = true,
-  filters = {
-    dotfiles = true,
-    custom = {},
-    exclude = {},
+return {
+  "nvim-tree/nvim-tree.lua",
+  version = "*",
+  lazy = false,
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
   },
-  open_on_tab = false,
-  hijack_cursor = false,
-  update_cwd = true,
-  hijack_directories = {
-    enable = true,
-    auto_open = true,
-  },
-  diagnostics = {
-    enable = true,
-    icons = {
-      hint = "",
-      info = "",
-      warning = "",
-      error = "",
-    },
-  },
-  update_focused_file = {
-    enable = true,
-    update_cwd = true,
-    ignore_list = {},
-  },
-  git = {
-    enable = true,
-    ignore = false,
-    timeout = 500,
-  },
-  actions = {
-    use_system_clipboard = true,
-    change_dir = {
-      enable = false,
-      global = false,
-      restrict_above_cwd = false,
-    },
-    open_file = {
-      quit_on_open = true,
-      resize_window = true,
-      window_picker = {
-        enable = false,
-        chars = "1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-        exclude = {
-          filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame" },
-          buftype = { "nofile", "terminal", "help" },
+  config = function()
+    require("nvim-tree").setup {
+      on_attach = on_attach,
+      disable_netrw = true,
+      hijack_netrw = true,
+      filters = {
+        dotfiles = true,
+        custom = {},
+        exclude = {},
+      },
+      open_on_tab = false,
+      hijack_cursor = false,
+      update_cwd = true,
+      hijack_directories = {
+        enable = true,
+        auto_open = true,
+      },
+      diagnostics = {
+        enable = true,
+        icons = {
+          hint = "",
+          info = "",
+          warning = "",
+          error = "",
         },
       },
-    },
-  },
-   view = {
-    side = "left",
-    number = false,
-    relativenumber = false,
-    float = {
-      enable = false,
-      open_win_config = function()
-        local screen_w = vim.opt.columns:get()
-        local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
-        local window_w = screen_w * WIDTH_RATIO
-        local window_h = screen_h * HEIGHT_RATIO
-        local window_w_int = math.floor(window_w)
-        local window_h_int = math.floor(window_h)
-        local center_x = (screen_w - window_w) / 2
-        local center_y = ((vim.opt.lines:get() - window_h) / 2)
-                         - vim.opt.cmdheight:get()
-        return {
-          border = 'rounded',
-          relative = 'editor',
-          row = center_y,
-          col = center_x,
-          width = window_w_int,
-          height = window_h_int,
-        }
+      update_focused_file = {
+        enable = true,
+        update_cwd = true,
+        ignore_list = {},
+      },
+      git = {
+        enable = true,
+        ignore = false,
+        timeout = 500,
+      },
+      actions = {
+        use_system_clipboard = true,
+        change_dir = {
+          enable = false,
+          global = false,
+          restrict_above_cwd = false,
+        },
+        open_file = {
+          quit_on_open = true,
+          resize_window = true,
+          window_picker = {
+            enable = false,
+            chars = "1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+            exclude = {
+              filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame" },
+              buftype = { "nofile", "terminal", "help" },
+            },
+          },
+        },
+      },
+      view = {
+        side = "left",
+        number = false,
+        relativenumber = false,
+        float = {
+          enable = false,
+          open_win_config = function()
+            local screen_w = vim.opt.columns:get()
+            local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+            local window_w = screen_w * WIDTH_RATIO
+            local window_h = screen_h * HEIGHT_RATIO
+            local window_w_int = math.floor(window_w)
+            local window_h_int = math.floor(window_h)
+            local center_x = (screen_w - window_w) / 2
+            local center_y = ((vim.opt.lines:get() - window_h) / 2)
+            - vim.opt.cmdheight:get()
+            return {
+              border = 'rounded',
+              relative = 'editor',
+              row = center_y,
+              col = center_x,
+              width = window_w_int,
+              height = window_h_int,
+            }
+          end,
+        },
+        width = function()
+          return math.floor(vim.opt.columns:get() * WIDTH_RATIO)
         end,
-    },
-    width = function()
-      return math.floor(vim.opt.columns:get() * WIDTH_RATIO)
-    end,
-  },
-  renderer = {
-    -- root_folder_label = false,
-    icons = {
-      glyphs = {
-        default = "",
-        symlink = "",
-        git = {
-          unstaged = "",
-          staged = "S",
-          unmerged = "",
-          renamed = "➜",
-          deleted = "",
-          untracked = "U",
-          ignored = "◌",
-        },
-        folder = {
-          default = "",
-          open = "",
-          empty = "",
-          empty_open = "",
-          symlink = "",
-        },
+      },
+      renderer = {
+        -- root_folder_label = false,
+        icons = {
+          glyphs = {
+            default = "",
+            symlink = "",
+            git = {
+              unstaged = "",
+              staged = "S",
+              unmerged = "",
+              renamed = "➜",
+              deleted = "",
+              untracked = "U",
+              ignored = "◌",
+            },
+            folder = {
+              default = "",
+              open = "",
+              empty = "",
+              empty_open = "",
+              symlink = "",
+            },
+          }
+        }
       }
+
+
     }
-  }
+  end,
 }
