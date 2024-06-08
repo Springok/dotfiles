2008142008de08eca79712cb347e2e13f3df116e
Wenchuan Lin <wenchuan.lin.tw@gmail.com>
2024-06-09 00:34:44 +0800

    Add oil

---
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "stevearc/oil.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("oil").setup {
+        default_file_explorer = false,
+        columns = { "icon" },
+        keymaps = {
+          ["<C-h>"] = false,
+          ["<M-h>"] = "actions.select_split",
+        },
+        view_options = {
+          show_hidden = true,
+        },
+      }
+
+      -- Open parent directory in current window
+      vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+
+      -- Open parent directory in floating window
+      vim.keymap.set("n", "<space>-", require("oil").toggle_float)
+    end,
+  },
+}
