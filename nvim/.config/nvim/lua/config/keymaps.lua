3905eab1576712575f641acf5e5b98069d600e1f
Wenchuan Lin <wenchuan.lin.tw@gmail.com>
2024-06-20 17:24:55 +0800

    Remove leader setup, and keep the one in init.lua

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -7,10 +7,6 @@ local term_opts = { silent = true }
 -- Shorten function name
 local keymap = vim.api.nvim_set_keymap
 
--- Remap leader key
-vim.g.mapleader = ","
--- vim.g.maplocalleader = " "
-
 --  discard direction keys
 keymap("n", "<up>", "<nop>", opts)
 keymap("n", "<down>", "<nop>", opts)
