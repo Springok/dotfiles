779911bac310aae272e2cffd843993ca07cd169c
Wenchuan Lin <wenchuan.lin.tw@gmail.com>
2024-10-15 17:55:23 +0800

    Remove obsoleted config

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -90,10 +90,6 @@ return {
   {
     "folke/which-key.nvim",
     event = "VeryLazy",
-    init = function()
-      vim.o.timeout = true
-      vim.o.timeoutlen = 500
-    end,
     opts = {
       -- your configuration comes here
       -- or leave it empty to use the default settings
