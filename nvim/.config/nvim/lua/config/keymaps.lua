df44a6259431625ce213740e1acf9f1f07e92af3
Wenchuan Lin <wenchuan.lin.tw@gmail.com>
2024-11-21 16:23:47 +0800

    Fix code format in lua

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -62,9 +62,9 @@ vim.keymap.set("n", "<leader>kr", ":VtrKillRunner<CR>")
 vim.keymap.set("n", "<leader>ur", ":VtrUnsetRunnerPane<CR>")
 vim.keymap.set("n", "<leader>sl", ":VtrSendLinesToRunner<CR>")
 vim.keymap.set(
-	"n",
-	"<leader>rc",
-	":VtrUnsetRunnerPane<CR>:VtrOpenRunner {'orientation': 'v', 'percentage': 15, 'cmd': 'rc'}<CR>"
+    "n",
+    "<leader>rc",
+    ":VtrUnsetRunnerPane<CR>:VtrOpenRunner {'orientation': 'v', 'percentage': 15, 'cmd': 'rc'}<CR>"
 )
 
 -- start interactive EasyAlign in visual mode
@@ -81,10 +81,10 @@ keymap("n", "<leader>fl", "<cmd>lua require('telescope.builtin').find_files()<cr
 keymap("n", "<leader>fh", "<cmd>lua require('telescope.builtin').help_tags()<cr>", opts)
 keymap("n", "<leader>ff", "<cmd>lua require('telescope.builtin').live_grep()<cr>", opts)
 keymap(
-	"n",
-	"<leader>fo",
-	"<cmd>lua require('telescope.builtin').live_grep({prompt_title = 'find string in open buffers...', grep_open_files=true})<cr>",
-	opts
+    "n",
+    "<leader>fo",
+    "<cmd>lua require('telescope.builtin').live_grep({prompt_title = 'find string in open buffers...', grep_open_files=true})<cr>",
+    opts
 )
 keymap("n", "<leader>fc", "<cmd>lua require('telescope.builtin').grep_string()<cr>", opts)
 keymap("n", "<leader>dl", "<cmd>lua require('telescope.builtin').diagnostics()<cr>", opts)
