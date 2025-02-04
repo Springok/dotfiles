0634307c6957035d542bb01dd995fd854bb93b53
Wenchuan Lin <wenchuan.lin.tw@gmail.com>
2025-02-04 10:50:01 +0800

    Capture the code screenshot right from the editor

--- nvim/.config/nvim/lua/plugins/util.lua
+++ nvim/.config/nvim/lua/plugins/util.lua
@@ -4,4 +4,19 @@ return {
 
   -- Remove spacing
   "McAuleyPenney/tidy.nvim",
+  {
+    "mistricky/codesnap.nvim",
+    config = function()
+      require("codesnap").setup({
+        bg_theme = "bamboo",
+        bg_padding = 0,
+        watermark = nil,
+        mac_window_bar = false,
+        -- has_breadcrumbs = true,
+        -- show_workspace = true,
+        -- has_line_number = true,
+      })
+    end,
+    build = "make"
+  },
 }
