876fda7da3538d9bc4b6b54a91a061fd63db2e70
Wenchuan Lin <wenchuan.lin.tw@gmail.com>
2025-02-04 10:49:26 +0800

    Colorize the todo / bug / fixme comment

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -95,5 +95,14 @@ return {
       -- or leave it empty to use the default settings
       -- refer to the configuration section below
     },
+  },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
   }
 }
