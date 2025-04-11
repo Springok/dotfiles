90135f7683950225d5e8c7ec6b1a45b0ef8707c8
Wenchuan Lin <wenchuan.lin.tw@gmail.com>
2025-04-11 15:50:11 +0800

    Update treesitter-context to be no limit
    Also remove unfounded module "lazyvim.util", which is gone for unknown
    reason...
    
    Reference
     - https://github.com/nvim-treesitter/nvim-treesitter-context?tab=readme-ov-file#configuration

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -145,19 +145,13 @@ return {
     "nvim-treesitter/nvim-treesitter-context",
     -- event = "LazyFile",
     enabled = true,
-    opts = { mode = "cursor", max_lines = 3 },
+    opts = { mode = "cursor", max_lines = 0 },
     keys = {
       {
         "<leader>ut",
         function()
-          local Util = require("lazyvim.util")
           local tsc = require("treesitter-context")
           tsc.toggle()
-          if Util.inject.get_upvalue(tsc.toggle, "enabled") then
-            Util.info("Enabled Treesitter Context", { title = "Option" })
-          else
-            Util.warn("Disabled Treesitter Context", { title = "Option" })
-          end
         end,
         desc = "Toggle Treesitter Context",
       },
