9b1bf7df9665c5d66cc989bcfa4f020db67c700c
Wenchuan Lin <wenchuan.lin.tw@gmail.com>
2025-04-11 17:27:16 +0800

    Extra fix for package install in lsp (lua related)

--- nvim/.config/nvim/lua/config/lsp/init.lua
+++ nvim/.config/nvim/lua/config/lsp/init.lua
@@ -67,3 +67,16 @@ for _, server in pairs(servers) do
   end
   lspconfig[server].setup(opts)
 end
+
+local registry = require 'mason-registry'
+for _, package_name in ipairs { 'lua-language-server', 'stylua' } do
+  local ok, package = pcall(function()
+    return registry.get_package(package_name)
+  end)
+  if ok and (not package:is_installed()) then
+    -- Override the installation options
+    package:install {
+      target = 'linux_arm64_gnu', -- Specify the target platform manually if supported
+    }
+  end
+end
