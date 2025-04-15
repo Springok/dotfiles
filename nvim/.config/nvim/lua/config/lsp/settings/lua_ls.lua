004e0903350a21d62c76108b7b662351a0e4fdec
Wenchuan L. <wenchuan.lin.tw@gmail.com>
2025-04-15 17:37:37 +0800

    Fix incorrect nested setting in lua_ls
    Reference:
      - https://github.com/neovim/neovim/discussions/24119

--- nvim/.config/nvim/lua/config/lsp/settings/lua_ls.lua
+++ nvim/.config/nvim/lua/config/lsp/settings/lua_ls.lua
@@ -1,18 +1,16 @@
 return {
   settings = {
-    lua_ls = {
-      cmd = { '/download/lua-language-server-3.13.5-linux-arm64/bin/lua-language-server' },
-      -- filetypes = { ... },
-      -- capabilities = {},
-      settings = {
-        Lua = {
-          completion = {
-            callSnippet = 'Replace',
-          },
-          -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-          -- diagnostics = { disable = { 'missing-fields' } },
+    Lua = {
+      diagnostics = {
+        globals = { "vim", "use" },
+      },
+      workspace = {
+        library = {
+          [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+          [vim.fn.stdpath("config") .. "/lua"] = true,
         },
       },
     },
   },
 }
+
